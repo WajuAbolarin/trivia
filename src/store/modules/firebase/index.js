674b1db074a8b1671dabcce5ce207010ddfb8633
Abolarin Olanrewaju Olabode <waju.abolarin@gmail.com>
2018-12-24 14:49:18 +0100

    Remove sensitive keys

--- src/store/modules/firebase/index.js
+++ src/store/modules/firebase/index.js
@@ -2,10 +2,10 @@ import * as firebase from 'firebase';
 
 firebase.initializeApp(
     {
-      apiKey: 'AIzaSyCMewMSS9L5YOQlonFIzpGOEi321u8Wdqo',
-      authDomain: 'waju-trivia.firebaseapp.com',
-      databaseURL: 'https://waju-trivia.firebaseio.com',
-      projectId: 'waju-trivia',
+      apiKey: process.env.VUE_APP_FIREBASE_KEY,
+      authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+      projectId: process.env.VUE_APP_FIREBASE_ID,
     }
 )
 
