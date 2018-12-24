af000cc238cebb07b771c00ec286e5387e46aef6
Abolarin Olanrewaju Olabode <waju.abolarin@gmail.com>
2018-12-24 21:25:51 +0100

    d

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app" class="app-wrapper">
+    <router-view class="view" />
+    <footer-menu></footer-menu>
+  </div>
+</template>
+
+<script>
+import FooterMenu from '@/components/FooterMenu.vue'
+
+export default {
+  components: {
+    FooterMenu
+  },
+}
+</script>
+<style>
+ @import url('./assets/framework.css');
+ .app-wrapper{
+   display: flex;
+   flex-direction: column;
+   min-height: 100vh;
+ }
+ .view{
+   flex: 1;
+ }
+</style>
